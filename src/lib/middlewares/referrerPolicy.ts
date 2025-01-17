21793967a8b8360b629564df325560952619d648
fengmk2 <suqian.yf@antgroup.com>
2025-01-18 00:55:19 +0800

    Update src/lib/middlewares/referrerPolicy.ts
    Co-authored-by: coderabbitai[bot] <136622811+coderabbitai[bot]@users.noreply.github.com>

--- src/lib/middlewares/referrerPolicy.ts
+++ src/lib/middlewares/referrerPolicy.ts
@@ -31,7 +31,7 @@ export default (options: SecurityConfig['referrerPolicy']) => {
 
     const policy = opts.value;
     if (!ALLOWED_POLICIES_ENUM.includes(policy)) {
-      throw new Error('"' + policy + '" is not available."');
+      throw new Error('"' + policy + '" is not available.');
     }
 
     ctx.set('referrer-policy', policy);
